836684036f3a8c4d783f9f6536c4837d61fbd9f0
kasperstuck <kasperstuck@gmail.com>
2026-04-18 16:34:24 +0200

    Replace Nordnet banners with Saxo Bank banner

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -1,22 +1,19 @@
-import livrenteKvinde from "@/assets/partners/nordnet-livrente-kvinde.png";
-import livrenteMand from "@/assets/partners/nordnet-livrente-mand.png";
-import mindreaarig from "@/assets/partners/nordnet-mindreaarig.png";
 import norminvestKvinder from "@/assets/partners/norminvest-gratis-investeringsplan-kvinder.png";
 import norminvestPar from "@/assets/partners/norminvest-gratis-investeringsplan-par.png";
+import saxoBedste from "@/assets/partners/saxo-bedste-investeringsplatform.png";
 
-const NORDNET_EXCLUDE = [
+const NORMINVEST_EXCLUDE = [
 	"saxo",
 	"lunar",
-	"norm invest",
-	"norminvest",
 	"maj invest",
 	"majinvest",
 	"june",
 	"frinans",
 ];
-const NORMINVEST_EXCLUDE = [
-	"nordnet",
-	"saxo",
+
+const SAXO_EXCLUDE = [
+	"norm invest",
+	"norminvest",
 	"lunar",
 	"maj invest",
 	"majinvest",
@@ -25,27 +22,6 @@ const NORMINVEST_EXCLUDE = [
 ];
 
 export const banners = [
-	{
-		image: livrenteMand,
-		alt: "Nordnet - Livsvarig pension, flyt din livrente",
-		href: "https://go.adt212.net/t/t?a=1930764499&as=2056026217&t=2&tk=1",
-		keywords: ["pension", "livrente", "ratepension", "aldersopsparing", "mand"],
-		exclude: NORDNET_EXCLUDE,
-	},
-	{
-		image: livrenteKvinde,
-		alt: "Nordnet - Invester selv din pension, flyt din livrente",
-		href: "https://go.adt212.net/t/t?a=1930763698&as=2056026217&t=2&tk=1",
-		keywords: ["pension", "livrente", "ratepension", "aldersopsparing", "kvinde", "hende", "dame"],
-		exclude: NORDNET_EXCLUDE,
-	},
-	{
-		image: mindreaarig,
-		alt: "Nordnet - Opret en konto for mindreårige",
-		href: "https://go.adt212.net/t/t?a=1886842299&as=2056026217&t=2&tk=1",
-		keywords: ["børn", "mindreårig", "børneopsparing", "barn"],
-		exclude: NORDNET_EXCLUDE,
-	},
 	{
 		image: norminvestPar,
 		alt: "Norm Invest - Den nemmeste måde at investere, gratis investeringsplan",
@@ -59,5 +35,12 @@ export const banners = [
 		href: "https://at.norminvest.com/t/t?a=1873806210&as=2056026217&t=2&tk=1",
 		keywords: [],
 		exclude: NORMINVEST_EXCLUDE,
+	},
+	{
+		image: saxoBedste,
+		alt: "Saxo Bank - Dansk investeringsplatform med handel i aktier, ETF'er, obligationer og fonde",
+		href: "https://go.adt212.net/t/t?a=1937587093&as=2056026217&t=2&tk=1",
+		keywords: ["investering", "aktier", "etf", "portefølje", "kurtage", "platform", "obligationer"],
+		exclude: SAXO_EXCLUDE,
 	},
 ];
